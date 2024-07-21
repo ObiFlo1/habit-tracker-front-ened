c450d8bc2c98edc73bcec04b11112a91df674368
ObiFlo1 <rubflores1000@gmail.com>
2024-07-21 10:08:58 -0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <div>hi</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
